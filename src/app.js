cbc05a6bd3685c386b77fd915803fa7fbeee80ca
Vinay Sudani <vinaysudani9@gmail.com>
2020-08-22 10:23:26 +0530

    Enable CORS

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
