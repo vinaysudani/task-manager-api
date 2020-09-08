3cd4b872ab60e098490263e4191ea7ba4dd7343b
Vinay Sudani <vinaysudani9@gmail.com>
2020-09-08 10:29:51 +0530

    Updated Unauthorized message

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -15,7 +15,7 @@ const auth = async (req, res, next) => {
         req.user = user
         next()
     } catch (e) {
-        res.status(401).send({ message: 'Please authenticate' })
+        res.status(401).send({ message: 'Unauthorized' })
     }
 }
 
